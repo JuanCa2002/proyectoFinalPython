45c3c5f3b362d5b6bff38e37544ecca50d9f2f40
Dajho428 <89399827+Dajho428@users.noreply.github.com>
2021-11-23 11:47:50 -0500

    Ojala no la cague

---
+++ local_settings.py
@@ -0,0 +1,11 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'la_tienda_de_mascotas.sqlite'),
+    }
+}
+
+DEBUG = True
